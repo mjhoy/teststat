b1c6646ab86f8a9b3a3048c27c76140208d6fe6a
Michael Hoy <michael.john.hoy@gmail.com>
2011-05-12 09:58:56 -0400

    CodeStat class to parse MiniTest output

---
+++ lib/codestat.rb
@@ -0,0 +1,29 @@
+class CodeStat
+
+  attr_accessor :data
+
+  # Minitest output:
+  # 1 tests, 1 assertions, 1 failures, 0 errors, 0 skips
+  TEST_REGEX = /(\d) tests?, (\d) assertions?, (\d) failures?, (\d) errors?/
+
+  def initialize(res)
+    ts = Time.now
+    tests, assertions, failures, errors =
+      res.match(TEST_REGEX).captures
+    @data = {
+      :tests => tests.to_i,
+      :assertions => assertions.to_i,
+      :failures => failures.to_i,
+      :errors => errors.to_i,
+      :timestamp => ts
+    }
+  end
+
+  def output_stats
+    failures, errors = @data[:failures], @data[:errors]
+    puts "\n"
+    puts "-----"
+    puts "CAPTURING TEST (stats): #{failures} failures and #{errors} errors."
+  end
+
+end
---
+++ test/codestat_test.rb
@@ -0,0 +1,34 @@
+$: << File.expand_path(File.dirname(__FILE__) + "/../lib/")
+require 'codestat'
+require 'minitest/autorun'
+
+class CodeStatTest < MiniTest::Unit::TestCase
+
+  def test_loads
+    assert CodeStat
+  end
+
+  def test_analyze_output
+    output = Proc.new do |d|
+      <<-FOO
+Loaded suite test_test
+Started
+.
+Finished in 0.000806 seconds.
+
+1 tests, 1 assertions, #{d[:failures]} failures, #{d[:errors]} errors, 0 skips
+
+Test run options: --seed 25626
+    FOO
+    end
+
+    stat1 = CodeStat.new(output.call(:failures => 0, :errors => 0))
+    assert_equal 0, stat1.data[:failures]
+    assert_equal 0, stat1.data[:errors]
+
+    stat2 = CodeStat.new(output.call(:failures => 1, :errors => 2))
+    assert_equal 1, stat2.data[:failures]
+    assert_equal 2, stat2.data[:errors]
+  end
+
+end
