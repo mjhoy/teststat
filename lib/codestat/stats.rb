05c43375163676de3ad748760dd2f97b1226314c
Michael Hoy <michael.john.hoy@gmail.com>
2011-05-15 23:09:22 -0400

    stats on the exit status

--- lib/codestat/stats.rb
+++ lib/codestat/stats.rb
@@ -11,18 +11,30 @@ module CodeStat
     def stat(key)
       if key == :success
         success_stat
+      elsif key == :badexits
+        bad_exit_stat
       else
-        d = { :sum => 0 }
+        d = { :sum => 0, :any => 0 }
         @stats.each { |s| d[:sum] += (s[key] || 0) }
         d[:mean] = d[:sum].to_f / @total_runs
         d
       end
     end
 
+    def bad_exit_stat
+      d = { :sum => 0 }
+      @stats.each do |s| 
+        d[:sum] += 1 unless s[:exitstatus] == 0
+      end
+      d
+    end
+
     def success_stat
       d = { :sum => 0 }
       @stats.each do |s| 
-        d[:sum] += 1 unless ((s[:errors] || 0) + (s[:failures] || 0)) > 0
+        d[:sum] += 1 unless ((s[:errors] || 0) + 
+                             (s[:failures] || 0) +
+                             (s[:exitstatus] || 0)) > 0
       end
       d[:mean] = d[:sum].to_f / @total_runs
       d
@@ -32,6 +44,7 @@ module CodeStat
       buf = "Out of #{@total_runs} total runs, " +  
         "you had #{stat(:errors)[:sum]} errors, " +
         "#{stat(:failures)[:sum]} failures, " +
+        "#{stat(:badexits)[:sum]} bad exits, " +
         "and #{stat(:success)[:sum]} successes."
     end
 
--- test/test_stats.rb
+++ test/test_stats.rb
@@ -5,18 +5,17 @@ class StatsTest < MiniTest::Unit::TestCase
 
   def setup
     @data = [
-      { :errors => 0, :failures => 1 },
-      { :errors => 1, :failures => 1 },
-      { :errors => 0, :failures => 0 }
+      { :errors => 0, :failures => 1, :exitstatus => 0 },
+      { :errors => 1, :failures => 1, :exitstatus => 0 },
+      { :errors => 0, :failures => 0, :exitstatus => 0 },
+      { :errors => 0, :failures => 0, :exitstatus => 1 },
     ]
 
   end
 
   def test_total_runs
-
     s = CodeStat::Stats.new(@data)
-    assert_equal 3, s.total_runs
-
+    assert_equal 4, s.total_runs
   end
 
   def test_stat
@@ -24,7 +23,7 @@ class StatsTest < MiniTest::Unit::TestCase
     error_stat = s.stat(:errors)
 
     assert_equal 1, error_stat[:sum]
-    assert_equal 1.0/3, error_stat[:mean]
+    assert_equal 1.0/4, error_stat[:mean]
   end
 
   def test_successes
@@ -32,7 +31,7 @@ class StatsTest < MiniTest::Unit::TestCase
     success_stat = s.stat(:success)
 
     assert_equal 1, success_stat[:sum]
-    assert_equal 1.0/3, success_stat[:mean]
+    assert_equal 1.0/4, success_stat[:mean]
   end
 
   def test_print_stats
@@ -41,11 +40,21 @@ class StatsTest < MiniTest::Unit::TestCase
 
     out = s.print_stats
 
-    assert_match /out of 3 total runs/i, out
+    assert_match /out of 4 total runs/i, out
     assert_match /1 errors/i, out
     assert_match /2 failures/i, out
+    assert_match /1 bad exits/i, out
     assert_match /1 successes/i, out
+  end
 
+  def test_other_exit_statuses
+    data = [
+      { :errors => 0, :failures => 1, :exitstatus => 1 },
+      { :errors => 1, :failures => 1, :exitstatus => 3 },
+    ]
+    s = CodeStat::Stats.new(data)
+    out = s.print_stats
+    assert_match /2 bad exits/i, out
   end
 
 end
